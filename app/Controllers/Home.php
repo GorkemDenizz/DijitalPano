70228ec5cd58cbfe6452466277d17f3bf935cdfb
YusufDurmaz <ydurmaz2021@gmail.com>
2026-02-08 20:26:23 +0300

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,15 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        $configModel = new \App\Models\AppConfig();
-        $data = $configModel->getConfigValues(["school_name"]);
+        $configModel = new \App\Models\AppConfigModel();
+        $schoolName = $configModel->getConfigValues(["school_name"]);
+        $title = lang("App.dashboard");
+
+        $data = [
+            "school_name" => $schoolName,
+            "title" => $title,
+        ];
+
         return view("dashboard/index", $data);
     }
 }
